bcafe217ad4d824b9e3ff5dfe0253082be4bbc97
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-12-25 02:59:24 +0300

    Scripted auto-commit on change (2020-12-25 02:59:24) by gitwatch.sh

--- src/main/java/bmstu/Storage.java
+++ src/main/java/bmstu/Storage.java
@@ -33,7 +33,7 @@ public class Storage {
             if (storage.pollin(0)){
                 ZMsg messageFromProxy = ZMsg.recvMsg(backend);
                 messageFromProxy.unwrap();
-                
+
             }
         }
     }
