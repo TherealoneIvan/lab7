6a9c3730254fb1d71265fc2f47f9272422461fc2
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-12-25 01:06:09 +0300

    Scripted auto-commit on change (2020-12-25 01:06:09) by gitwatch.sh

--- src/main/java/bmstu/Storage.java
+++ src/main/java/bmstu/Storage.java
@@ -4,6 +4,7 @@ import org.zeromq.SocketType;
 import org.zeromq.ZMQ;
 
 public class Storage {
+    
     public static void main(String[] args){
         ZMQ.Context context = ZMQ.context(1);
         ZMQ.Socket backend =
