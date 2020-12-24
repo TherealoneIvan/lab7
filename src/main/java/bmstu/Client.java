54747e618cc5c5f82b3e0de931b18047ae4b4b83
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-12-24 23:35:48 +0300

    Scripted auto-commit on change (2020-12-24 23:35:48) by gitwatch.sh

--- src/main/java/bmstu/Client.java
+++ src/main/java/bmstu/Client.java
@@ -3,13 +3,10 @@ package bmstu;
 import org.zeromq.SocketType;
 import org.zeromq.ZMQ;
 
-
-import java.net.Socket;
-
 public class Client {
     public static void main(String[] args){
         ZMQ.Context context = ZMQ.context(1);
-        Socket requester =
+        ZMQ.Socket requester =
                 context.socket(SocketType.REQ);
         requester.connect("tcp://localhost:5559");
         System.out.println("launch and connect client.");
