aa6b49371358616873cd0f8a69072199da1a94b5
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-12-25 02:20:45 +0300

    Scripted auto-commit on change (2020-12-25 02:20:45) by gitwatch.sh

--- src/main/java/bmstu/Storage.java
+++ src/main/java/bmstu/Storage.java
@@ -31,7 +31,7 @@ public class Storage {
                 startTime = System.currentTimeMillis();
             }
             if (storage.pollin(0)){
-                
+                ZMsg = 
             }
         }
     }
