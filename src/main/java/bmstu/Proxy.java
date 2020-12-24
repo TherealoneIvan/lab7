e3b8798334c3f706833e340ffbd1db937d7d01a3
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-12-24 23:46:18 +0300

    Scripted auto-commit on change (2020-12-24 23:46:18) by gitwatch.sh

--- src/main/java/bmstu/Proxy.java
+++ src/main/java/bmstu/Proxy.java
@@ -1,7 +1,28 @@
 package bmstu;
 
+
+import org.zeromq.SocketType;
+import org.zeromq.ZMQ;
+
 public class Proxy {
     public static void main(String[] args){
-        
+        ZMQ.Context context = ZMQ.context(1);
+        ZMQ.Socket frontend =
+                context.socket(SocketType.ROUTER);
+        ZMQ.Socket backend =
+                context.socket(SocketType.DEALER);
+        frontend.bind("tcp://*:5559");
+        backend.bind("tcp://*:5560");
+        System.out.println("launch and connect broker.");
+
+        ZMQ.Poller items = context.poller (2);
+        items.register(frontend, ZMQ.Poller.POLLIN);
+        items.register(backend, ZMQ.Poller.POLLIN);
+        boolean more = false;
+        byte[] message;
+
+        while (!Thread.currentThread().isInterrupted()) {
+            
+        }
     }
 }
