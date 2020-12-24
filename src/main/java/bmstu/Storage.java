df72c1fc510802726b3ab49889f59c60e87ec337
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-12-25 01:16:36 +0300

    Scripted auto-commit on change (2020-12-25 01:16:36) by gitwatch.sh

--- src/main/java/bmstu/Storage.java
+++ src/main/java/bmstu/Storage.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class Storage {
     Scanner input = new Scanner(System.in);
-    
+    String arguments = 
     public static void main(String[] args){
         ZMQ.Context context = ZMQ.context(1);
         ZMQ.Socket backend =
