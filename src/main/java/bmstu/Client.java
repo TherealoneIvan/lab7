b905c79633437d982b83635d1ceca44d0ad20cc2
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-12-24 23:21:01 +0300

    Scripted auto-commit on change (2020-12-24 23:21:01) by gitwatch.sh

--- src/main/java/bmstu/Client.java
+++ src/main/java/bmstu/Client.java
@@ -1,7 +1,12 @@
 package bmstu;
 
+import javax.naming.Context;
+import java.net.Socket;
+
 public class Client {
     public static void main(String[] args){
-        
+        Context context = ZMQ.context(1);
+        Socket frontend =
+                context.socket(SocketType.ROUTER);
     }
 }
