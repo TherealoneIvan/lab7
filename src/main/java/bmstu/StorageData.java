e7a3ad7c689e8df2ce653f8dae07dba385b46412
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-12-25 03:24:08 +0300

    Scripted auto-commit on change (2020-12-25 03:24:08) by gitwatch.sh

--- src/main/java/bmstu/StorageData.java
+++ src/main/java/bmstu/StorageData.java
@@ -1,2 +1,37 @@
-package bmstu;public class StorageData {
+package bmstu;
+
+public class StorageData {
+    private Integer startSeq;
+    private Integer endSeq;
+    private long timeLife;
+
+    public StorageData(Integer startSeq, Integer endSeq, long timeLife) {
+        this.startSeq = startSeq;
+        this.endSeq = endSeq;
+        this.timeLife = timeLife;
+    }
+
+    public Integer getStartSeq() {
+        return startSeq;
+    }
+
+    public void setStartSeq(Integer startSeq) {
+        this.startSeq = startSeq;
+    }
+
+    public Integer getEndSeq() {
+        return endSeq;
+    }
+
+    public void setEndSeq(Integer endSeq) {
+        this.endSeq = endSeq;
+    }
+
+    public long getTimeLife() {
+        return timeLife;
+    }
+
+    public void setTimeLife(long timeLife) {
+        this.timeLife = timeLife;
+    }
 }
