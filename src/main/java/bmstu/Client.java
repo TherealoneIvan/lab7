e55cb93c7ac1f005fabc3357ff3fc730375d788e
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-12-25 03:08:29 +0300

    Scripted auto-commit on change (2020-12-25 03:08:29) by gitwatch.sh

--- src/main/java/bmstu/Client.java
+++ src/main/java/bmstu/Client.java
@@ -20,7 +20,7 @@ public class Client {
             req.add(msg);
             req.send(requester);
             ZMsg answ = ZMsg.recvMsg(requester);
-            
+            System.out.println(answ);
         }
     }
 }
