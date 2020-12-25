b79c9ce8b8b154c77826b6aea1928198c406e0d7
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-12-25 19:39:47 +0300

    Scripted auto-commit on change (2020-12-25 19:39:47) by gitwatch.sh

--- src/main/java/bmstu/Proxy.java
+++ src/main/java/bmstu/Proxy.java
@@ -58,7 +58,7 @@ public class Proxy {
                     message.pop();
                     int startSeq = Integer.parseInt(message.popString());
                     int endSeq = Integer.parseInt(message.popString());
-                    
+                    storageData.put()
                 }
             }
 
