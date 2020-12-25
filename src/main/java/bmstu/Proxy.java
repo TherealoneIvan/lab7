3e2dc5db507f477695a4d57041ffedfa27e12be3
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-12-25 19:57:47 +0300

    Scripted auto-commit on change (2020-12-25 19:57:47) by gitwatch.sh

--- src/main/java/bmstu/Proxy.java
+++ src/main/java/bmstu/Proxy.java
@@ -42,7 +42,7 @@ public class Proxy {
                 message = ZMsg.recvMsg(frontend);
                 String[] parsedMsg = message.toString().split("");
                 if (parsedMsg[COMMAND_ARGUMENT].equals(CLIENT_PUT_COMM)){
-                    for (Map.Entry<Z>){
+                    for (Map.Entry<ZFrame , StorageData> ){
                         if (data.getStartSeq() <= Integer.parseInt(parsedMsg[INPUT_DIGIT])
                                 && data.getEndSeq() >= Integer.parseInt(parsedMsg[INPUT_DIGIT])
                                 && System.currentTimeMillis() - data.getTimeLife() > LIFE_CICLE){
