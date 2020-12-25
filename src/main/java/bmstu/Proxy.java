c293cf6d83ad2760c099da86f1dbe39a44b99240
TherealoneIvan <52462417+TherealoneIvan@users.noreply.github.com>
2020-12-25 03:42:46 +0300

    Scripted auto-commit on change (2020-12-25 03:42:46) by gitwatch.sh

--- src/main/java/bmstu/Proxy.java
+++ src/main/java/bmstu/Proxy.java
@@ -8,8 +8,9 @@ import java.util.ArrayList;
 
 public class Proxy {
 
-    public static final int COMMAND_ARGUMENT = 1;
+    public static final int COMMAND_ARGUMENT = 0;
     public static final String CLIENT_PUT_COMM = "PUT";
+    public static final int INPUT_DIGIT = 1;
 
     public static void main(String[] args) {
         ArrayList<StorageData> storageData = new ArrayList<>();
@@ -35,7 +36,7 @@ public class Proxy {
                 String[] parsedMsg = message.toString().split("");
                 if (parsedMsg[COMMAND_ARGUMENT].equals(CLIENT_PUT_COMM)){
                     for (StorageData data : storageData){
-                        if (data.getStartSeq() <= )
+                        if (data.getStartSeq() <= parsedMsg[INPUT_DIGIT] && )
                     }
                 }
 
